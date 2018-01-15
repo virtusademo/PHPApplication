53895d49d191c34ef1f5fce326fa7c51b1a10853
AliRey <33079946+AliRey@users.noreply.github.com>
2018-01-15 14:25:06 +0400

    Update index.php

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello. Today is <?= date('l \t\h\e jS') ?>.
+            Hello Sushma. Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
