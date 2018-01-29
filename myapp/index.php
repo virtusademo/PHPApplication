b8fa2f730bd24eccc2f297fe823e1b2b055cb736
virtusademo <virtusademo@gmail.com>
2018-01-29 10:52:55 +0400

    Update index.php

--- myapp/index.php
+++ myapp/index.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <p>
-            Hello Sushma. Today is <?= date('l \t\h\e jS') ?>.
+            Hello Aliiii. Today is <?= date('l \t\h\e jS') ?>.
         </p>
     </body>
 </html>
